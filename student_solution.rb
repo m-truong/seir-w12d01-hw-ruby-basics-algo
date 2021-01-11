3e737b31f8e6ebaa72f2081bc626ad3915473efe
Michael Truong <michaeltruong@Michaels-MacBook-Pro.local>
2021-01-11 17:48:24 -0500

    Initial Commit
    Completed Get Started with a Few Reps

---
+++ student_solution.rb
@@ -0,0 +1,12 @@
+#### Get Started with a Few Reps Section ####
+
+# 1. Print "Hello World" to the console
+p "Hello World"
+
+# 2. Interpolate the adjective variable into the Hellow World string using a string literal
+adjective = "Big Bad"
+p "Hello #{adjective} World"
+
+# 3. Save the string "Hello World" to a variable. Without changing the letters in your code manually, use a string method to permanently change "Hello World" to uppercased letters. Print the value of the variable you created.
+hello = "Hello World" 
+p hello.upcase!
