9728d5af85d292e539ce0df60fbc4e1bcae6262c
Michael Truong <michaeltruong@Michaels-MacBook-Pro.local>
2021-01-11 20:13:08 -0500

    Colors Array Section completed

--- student_solution.rb
+++ student_solution.rb
@@ -1,24 +1,24 @@
-#### Get Started with a Few Reps Homework Section ####
+######## Get Started with a Few Reps Homework Section ########
 
 # 1. Print "Hello World" to the console
-# p "Hello World"
+p "Hello World"
 
 # 2. Interpolate the adjective variable into the Hellow World string using a string literal
 adjective = "Big Bad"
-# p "Hello #{adjective} World"
+p "Hello #{adjective} World"
 
 # 3. Save the string "Hello World" to a variable. Without changing the letters in your code manually, use a string method to permanently change "Hello World" to uppercased letters. Print the value of the variable you created.
 hello = "Hello World" 
-# p hello.upcase!
+p hello.upcase!
 
 
-#### Nums Array and Enumerables Homework Section ####
+######## Nums Array and Enumerables Homework Section ########
 
 nums = [5, 5, 6, 7, 2, 4, 3, 2, 1, 9, 7, 6, 0, 0, 35, 19, 66, 3, 22, 21, 4, 4, 4, 0, 1, 2, 3, 4, 12, 15]
 p nums
 
 # 1. Use .uniq to print the array with all duplicate entries removed
-# p nums.uniq
+p nums.uniq
 
 # 2. Next, use .push and .pop , .shift .unshift and .length on the array as you would with javaScript (if you need to add a number, add 5)
 p nums.push(100) # adds to end of nums-arr
@@ -67,3 +67,15 @@ p nums.reject { |v| v % 3 != 0}
 
 # 16. Use .select to return all the numbers divisible by 5
 p nums.select { |z| z % 5 == 0}
+
+######## Color Array Section ########
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+
+# 1. Print out a random color
+p colors.sample 
+
+# 2. Print out the colors array in reverse-order
+p colors.reverse
+
+# 3. Print out all of the colors in the array with all capital letters.
+p colors.each { |v| p v.upcase}
